b49b1e5ab9b5a57f76f19c46c38107c9b29d3d7d
Patricia Recarte <patriciarecarte@hotmail.com>
2020-06-11 17:10:49 +0100

    recoded form to add a dose, eliminating collection tag

--- app/controllers/doses_controller.rb
+++ app/controllers/doses_controller.rb
@@ -1,5 +1,4 @@
 class DosesController < ApplicationController
-
   def index
     @cocktail = Cocktail.find(params[cocktail_id])
     @doses = Cocktail.doses
@@ -12,12 +11,14 @@ class DosesController < ApplicationController
 
   def create
     @cocktail = Cocktail.find(params[:cocktail_id])
-    # 2.create the review with the strong params
     @dose = Dose.new(dose_strong_params)
-    # 3. we want to assign the review to the restaureant of the url
     @dose.cocktail_id = @cocktail.id
-    @dose.save!
-    redirect_to cocktail_path(@cocktail)
+
+    if @dose.save
+      redirect_to cocktail_path(@cocktail)
+    else
+      render :new
+    end
   end
 
   def destroy
@@ -33,5 +34,4 @@ class DosesController < ApplicationController
   def dose_strong_params
     params.require(:dose).permit(:description, :ingredient_id)
   end
-
 end
