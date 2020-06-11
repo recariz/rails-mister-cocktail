6cc7bb3af95235f2ac29479c9933394205d57932
Patricia Recarte <patriciarecarte@hotmail.com>
2020-06-11 15:09:56 +0100

    connected cocktails with ingredients and doses. Program working when creating a cocktail, showing all or a cocktail in particular, adding a dose with an ingredient chosen from a list, deleting a dose, and viewing each cocktail with its ingredients and doses

---
+++ app/controllers/cocktails_controller.rb
@@ -0,0 +1,25 @@
+class CocktailsController < ApplicationController
+  def index
+    @cocktails = Cocktail.all
+  end
+
+  def show
+    @cocktail = Cocktail.find(params[:id])
+    @doses = @cocktail.doses
+  end
+
+  def new
+    @cocktail = Cocktail.new
+  end
+
+  def create
+    @cocktail = Cocktail.new(strong_params)
+    @cocktail.save
+    redirect_to cocktails_path
+  end
+
+  private
+  def strong_params
+    params.require(:cocktail).permit(:name)
+  end
+end
