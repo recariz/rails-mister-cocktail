cedbda6989a03a64250dc76260a6cd116feac46e
Patricia Recarte <patriciarecarte@hotmail.com>
2020-06-12 17:46:54 +0200

    fixing heroku

--- app/controllers/cocktails_controller.rb
+++ app/controllers/cocktails_controller.rb
@@ -15,7 +15,7 @@ class CocktailsController < ApplicationController
   def create
     @cocktail = Cocktail.new(strong_params)
     @cocktail.save
-    redirect_to root_path
+    redirect_to root
   end
 
   private
