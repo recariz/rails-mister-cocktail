4efe5a448174a287c2a6845cafd10df5d2e54d40
Patricia Recarte <patriciarecarte@hotmail.com>
2020-06-12 18:27:55 +0200

    x

--- app/controllers/cocktails_controller.rb
+++ app/controllers/cocktails_controller.rb
@@ -14,8 +14,11 @@ class CocktailsController < ApplicationController
 
   def create
     @cocktail = Cocktail.new(strong_params)
-    @cocktail.save
-    redirect_to root_path
+    if @cocktail.save!
+      redirect_to "/"
+    else
+      render :new
+    end
   end
 
   private
